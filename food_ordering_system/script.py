b62a4e9097b2121ab206e815062ab26b32a11063
telkomdev-kevin <cosmoas13@gmail.com>
2021-02-11 14:42:16 +0700

    lesson 4

--- food_ordering_system/script.py
+++ food_ordering_system/script.py
@@ -1,17 +1,50 @@
-class MenuItem:
-    def info(self):
-        # Cetak dengan format '____: $____'
-        print(self.name + ": $" + str(self.price))
+from menu_item import MenuItem
 
+menu_item1 = MenuItem("Roti Lapis", 5)
+menu_item2 = MenuItem("Kue Coklat", 4)
+menu_item3 = MenuItem("Kopi", 3)
+menu_item4 = MenuItem("Jus Jeruk", 2)
 
-menu_item1 = MenuItem()
-menu_item1.name = "Roti Lapis"
-menu_item1.price = 5
+menu_items = [menu_item1, menu_item2, menu_item3, menu_item4]
 
-menu_item1.info()
+index = 0
 
-menu_item2 = MenuItem()
-menu_item2.name = "Kue Coklat"
-menu_item2.price = 4
+for menu_item in menu_items:
+    print(str(index) + ". " + menu_item.info())
+    index += 1
 
-menu_item2.info()
+print("--------------------")
+
+order = int(input("Masukkan nomor menu: "))
+selected_menu = menu_items[order]
+print("Item yang di pilih: " + selected_menu.name)
+
+# Terima input dari console, dan Berikan input ke variable count
+count = int(input("Jumlah pesanan (diskon 10% untuk 3 atau lebih): "))
+
+# Panggil method get_total_price
+result = selected_menu.get_total_price(count)
+
+# Cetak 'Total harga adalah $____'
+print("Total harga adalah $" + str(result))
+
+# ----------------------------------------------------------------------------
+
+# # Import class MenuItem dari menu_item.py
+# from menu_item import MenuItem
+
+# # Tambahkan 'Roti Lapis' dan 5 sebagai argument
+# menu_item1 = MenuItem("Roti Lapis", 5)
+
+# # Hapus dua baris di bawah tanpa menggunakan __init__
+# # name dan price harus dideklarasikan dengan variable instance
+# # menu_item1.name = 'Roti Lapis'
+# # menu_item1.price = 5
+
+# print(menu_item1.info())
+
+# # Panggil method get_total_price
+# result = menu_item1.get_total_price(4)
+
+# # Cetak 'Total harga adalah $____'
+# print("Total harga adalah $" + str(result))

54941f70ff4798b43aa1c2e9e859f6df8ca6df0e
telkomdev-kevin <cosmoas13@gmail.com>
2021-02-13 13:13:21 +0700

    lesson 5

--- ordering_system/script.py
+++ ordering_system/script.py
@@ -2,9 +2,44 @@ from food import Food
 from drink import Drink
 
 food1 = Food("Roti Lapis", 5, 330)
-print(food1.info())
+food2 = Food("Kue Coklat", 4, 450)
+food3 = Food("Kue Sus", 2, 180)
 
-# Tambahakan argument ke Drink()
+foods = [food1, food2, food3]
+
 drink1 = Drink("Kopi", 3, 180)
+drink2 = Drink("Jus Jeruk", 2, 350)
+drink3 = Drink("Espresso", 3, 30)
 
-print(drink1.info())
+drinks = [drink1, drink2, drink3]
+
+print("Makanan")
+index = 0
+for food in foods:
+    print(str(index) + ". " + food.info())
+    index += 1
+
+print("Minuman")
+index = 0
+for drink in drinks:
+    print(str(index) + ". " + drink.info())
+    index += 1
+
+print("--------------------")
+
+food_order = int(input("Masukkan nomor makanan: "))
+selected_food = foods[food_order]
+
+drink_order = int(input("Masukkan nomor minuman: "))
+selected_drink = drinks[drink_order]
+
+# Ambil input dari console dan tetapkan ke variable count
+count = int(input("Mau berapa paket makanan? (Diskon 10% untuk 3 atau lebih): "))
+
+# Panggil method get_total_price dari selected_food dan selected_drink
+f_price = selected_food.get_total_price(count)
+d_price = selected_drink.get_total_price(count)
+result = f_price + d_price
+
+# Cetak 'Total harga adalah $____'
+print("Total harga adalah $ " + str(result))
